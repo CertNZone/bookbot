d7873d5ee68009caf2b917f1cba9f38a4d0565a0
CertNZone <timothy.watters@outlook.com>
2025-04-14 20:12:47 +0800

    Added reporting

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_word_count
+from stats import get_word_count, num_characters, dict_sort
 
 def get_book_text(file_path):
     with open(file_path) as p:
@@ -8,7 +8,15 @@ def get_book_text(file_path):
 def main():
     contents = get_book_text("books/frankenstein.txt")
     length = get_word_count(contents)
-    
-    print(f"{length} words found in the document")
+    chars = num_characters(contents)
+    print("========= BOOKBOT =========")
+    print("Analyzing book found in books folder")
+    print("--------- Word Count ---------")
+    print(f"Found {length} total words")
+    print("--------- Character Count ---------")
+    report = dict_sort(chars)
+    for item in report:
+        if item['char'].isalpha():
+            print(f"{item['char']}: {item['num']}")
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,22 @@ def get_word_count(contents):
     split = contents.split()
     length = len(split)
     return length
+
+def num_characters(contents):
+    counted_chars = dict.fromkeys(contents.lower(), 0)
+    
+    for char in contents:
+        counted_chars[char.lower()] += 1
+
+    return counted_chars
+
+def dict_sort(input_dict):
+    dict_list = []
+    for item in input_dict:
+        dict_list.append({'char':item, 'num':input_dict[item]})
+    dict_list.sort(reverse=True, key=sort_on)
+
+    return dict_list
+
+def sort_on(dict):
+    return dict["num"]
